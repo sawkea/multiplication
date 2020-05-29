30345de552982fc1b6a6217cf3da154e3d9e2a45
Sawkea <shoareau@orange.fr>
2020-05-29 10:43:08 +0200

    struture header

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication</title>
+</head>
+<body>
+    <header>
+        <h1>Les tables de multiplications</h1>
+        <nav>
+            <li><a href="">Table X 3</a></li>
+            <li><a href="">Liste des tables</a></li>
+            <li><a href="">Choix des tables</a></li>
+            <li><a href="">Révision</a></li>
+            <li><a href="">Super mode révision</a></li>
+            <li><a href="">Révision avec Ajax</a></li>
+        </nav>
+    </header>
+
+
+
+</body>
+</html>
