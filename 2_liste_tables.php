6ae0c50eca82193dfaf11afb15eab566dd964cb7
Sawkea <shoareau@orange.fr>
2020-06-03 11:56:00 +0200

    updates file 2

--- 2_liste_tables.php
+++ 2_liste_tables.php
@@ -22,37 +22,60 @@
     <!-- Main -->
     <main>
         <?php
-
             echo "<h2>Choisir une table dans la liste</h2>";
         ?>
-
-        
-            
-
-            <form action="action.php" method="post">
-            <select name="demande">
-            <option selected="selected" value="table">Table N°</option>
-        
-            <p><input type="submit" value="OK"></p>
+            <form action="2_liste_tables.php" method="post">
+            <select name="table">
+            <option value="1">Table N°1</option>
+            <option value="2">Table N°2</option>
+            <option value="3">Table N°3</option>
+            <option value="4">Table N°4</option>
+            <option value="5">Table N°5</option>
+            <option value="6">Table N°6</option>
+            <option value="7">Table N°7</option>
+            <option value="8">Table N°8</option>
+            <option value="9">Table N°9</option>
+            <option value="10">Table N°10</option>
+            </select>
+            <input type="submit" value="OK">
             </form>
 
+         
+                <?php
+                // if ( (isset($_POST['table'])) {
+                // $choix = $_POST['table'];
+                // } else {
+                // $choix = 1; 
+                $choix = (isset($_POST['table'])) ? $_POST['table']: 1;
+                for( $j= 1; $j < 11; $j++){
+                    echo $choix . " X ". $j. " = ".($choix*$j).
+                     "<br/>";
+                    }
 
-        <?php
-            // Variable qui ajoutera l'attribut selected de la liste déroulante
-            $selected = '';
+                ?>
+                 <!-- for( $j= 2; $j < 11; $j++){
+                    echo $choix . " X ". $j. " = ".($choix*$j).
+                     "<br/>";
+                } -->
+      
+
+
+<!--         
+            // // Variable qui ajoutera l'attribut selected de la liste déroulante
+            // $selected = '';
     
-            // Parcours du tableau
-            echo '<select name="demande">',"\n";
-            for($i=1;$i<=10;$i++)
-            {
+            // // Parcours du tableau
+            // echo '<select name="demande">',"\n";
+            // for($i=1;$i<=10;$i++)
+            // {
         
-            // Affichage de la ligne
-            echo "\t",'<option value="', $i ,'"', $selected ,'>', $i ,'</option>',"\n";
-            // Remise à zéro de $selected
-            $selected='';
-            }
-            echo '</select>',"\n";
-            ?>
+            // // Affichage de la ligne
+            // echo "\t",'<option value="', $i ,'"', $selected ,'>', $i ,'</option>',"\n";
+            // // Remise à zéro de $selected
+            // $selected='';
+            // }
+            // echo '</select>',"\n";
+            //  -->
 
     </main>
            
