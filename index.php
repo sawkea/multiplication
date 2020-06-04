4afeb6074fe2647c5468222cb3ff543b7f7b3cf8
Sawkea <shoareau@orange.fr>
2020-06-04 15:53:23 +0200

    Mise en place des styles de la nav

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php
-    include 'header.html';
-    include 'fonction.php';
-
-    echo "<h2>Voici la table X 3</h2>";
-
-
-    // Afficher la table du 3
-    $nombre = 3; // variable "nombre" avec le paramètre 3
-		echo multiplication ($nombre); // afficher la fonction multi avec le paramètre 
+    <?php
+        include 'header.html';
+        include 'fonction.php';
     
+        echo "<h2>Voici la table X 3</h2>";
+    
+    
+        // Afficher la table du 3
+        $nombre = 3; // variable "nombre" avec le paramètre 3
+    		echo multiplication ($nombre); // afficher la fonction multi avec le paramètre 
+    ?>
+
     
     
     
