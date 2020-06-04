1fcd023f80bd1bfa5449619cbea974d47d616efb
Sawkea <shoareau@orange.fr>
2020-06-04 14:35:27 +0200

    Updates ajout de commentaires

--- fonction.php
+++ fonction.php
@@ -1,6 +1,7 @@
 <?php
+// création d'un fichier spécifique avec les fonctions plus simple pour des changements ou mises à jour éventuel
 
-// Création de la fonction de multiplication
+// Création de la fonction de multiplication utilisée dans les différentes pages
     function multiplication($nombre){ // nom de fontion "multi" avec son paramètre "nombre"
         for ($i = 1; $i <= 10; $i++){
             echo $nombre . " X " . $i . " = " . $nombre * $i . "<br>";
@@ -8,7 +9,7 @@
     }
 
 
-// fonction pour afficher les commentaires de résultat
+// fonction pour afficher les commentaires de résultat (dans la )page 4_révision)
 function super_revision($nombre, $resultat, $aleatoire)
 {
     echo "<br>" . $aleatoire . " X " . $nombre . " = " .$resultat . "<br><br>";
