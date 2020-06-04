75ca0bf419f5b6f6f0198c2ddfde0e720d361afd
Sawkea <shoareau@orange.fr>
2020-06-04 14:31:45 +0200

    Updates files avec l'include file function.php

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
     include 'header.html';
+    include 'fonction.php';
 
     echo "<h2>Voici la table X 3</h2>";
 
-    // Création de la fonction de multiplication
-    function multiplication($nombre){ // nom de fontion "multi" avec son paramètre "nombre"
-        for ($i = 1; $i <= 10; $i++){
-            echo $nombre . " X " . $i . " = " . $nombre * $i . "<br>";
-        }
-    }
+
     // Afficher la table du 3
     $nombre = 3; // variable "nombre" avec le paramètre 3
 		echo multiplication ($nombre); // afficher la fonction multi avec le paramètre 
