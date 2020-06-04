6b695ef6439a3b3a056af4a94d5b9a5870212dbd
Sawkea <shoareau@orange.fr>
2020-06-04 23:51:28 +0200

    updates page 6

--- 6_revision_ajax.php
+++ 6_revision_ajax.php
@@ -1,8 +1,48 @@
-<?php
-    include 'header.html';
-    include 'fonction.php';
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication</title>
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.scss">
+</head>
+<body>
+    <div class="container">
+        <!-- Header -->
+        <header>
+          <div class="flex between align_center">
+            <p><img src="img/logo_multi.svg" width="150" height="48" alt="Logo multi"></p>
+              <h1 class="font_title text_center color_title">Les tables de multiplication</h1>
+          </div>
+            <!-- Menu -->
+            <nav class="flex around align_center">
+                <li><a href="index.php">Table du 3</a></li>
+                <li><a href="2_liste_tables.php">Liste des tables</a></li>
+                <li><a href="3_choix_tables.php">Chexbox des tables</a></li>
+                <li><a href="4_revision.php">Révision</a></li>
+                <li><a href="5_super_revision.php">Super mode révision</a></li>
+                <li><a href="6_revision_ajax.php">Révision avec Ajax</a></li>    
+            </nav>
+        </header>
+    </div>
+    <main>
+        <section>
+
+              <div class="container text_center color">
+              <h2 class="font_title text_center color_title">Révision ajax</h2>
+                <?php
+                    include 'fonction.php';
+                ?>
+
+              </div>
+        </section>
     
-    echo "<h2>Révision ajax</h2>";
+    </main>
+
+</body>
+</html>   
 
 
 
